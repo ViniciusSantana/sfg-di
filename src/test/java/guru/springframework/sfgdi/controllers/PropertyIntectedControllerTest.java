b159fa50474e9f030fd385ff0614f50f97dea66b
Vinicius <vinicius.santana@trustly.com>
2020-03-14 16:10:00 -0300

    Create sample code for setter, property and contructor dependency injection

---
+++ src/test/java/guru/springframework/sfgdi/controllers/PropertyIntectedControllerTest.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PropertyIntectedControllerTest {
+    PropertyIntectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new PropertyIntectedController();
+        controller.greetingService = new GreetingServiceImpl();
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
